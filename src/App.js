fe13236eaffc052735109b00ed4d4bcc7402365b
Stephen Blair <stephenjohnblair@outlook.com>
2020-04-13 15:00:05 +0100

    Created Grid for game layout with basic form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Grid, Typography, Paper, Divider} from '@material-ui/core';
+import Form from './components/Form';
 
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to our React App</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Grid container style={{ height: "100vh"}} justify="center" alignItems="center">
+        <Grid item xs={3}>
+          <Paper style={{ padding: "50px" }} elevation={6}>
+            <Typography align="center" variant="h2" gutterBottom>HOT or COLD</Typography>
+            <Divider style={{ margin: "20px 0" }} />
+            <Form />
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TextField, Button } from '@material-ui/core';
+
+const Form = () => {
+    return (
+        <form>
+            <TextField style={{ paddingBottom: "20px" }} fullWidth type="number" name="guess" label="Enter your guess..." required />
+            <Button fullWidth variant="contained" color="primary" type="submit">Guess</Button>
+        </form>
+    )
+}
+
+export default Form;
